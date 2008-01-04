5e5b74ad2765a34a762f766bb24324a982fe29a7
Cheshire Team <cheshire@liv.ac.uk>
2008-01-04 12:19:59 +0000

    more diacritics added
    git-svn-id: http://cvs.cheshire3.org/repos/cheshire3/cheshire3/code@372 04eb1732-f344-4bcc-95d5-7f11b6d85e6b

--- preParser.py
+++ preParser.py
@@ -650,7 +650,7 @@ class CharacterEntityPreParser(PreParser):
             "percnt": '%'
             }
 
-        self.preEntities = {"OUML;" : "Ouml", "UUML" : "Uuml", "AELIG" : "AElig"}
+        self.preEntities = {"OUML;" : "Ouml", "UUML" : "Uuml", "AELIG" : "AElig", "Aelig" : "AElig"}
         self.entities = ['nbsp', 'iexcl', 'cent', 'pound', 'curren', 'yen', 'brvbar', 'sect', 'uml',    'copy',   'ordf',   'laquo',  'not',    'shy',    'reg',    'macr',   'deg',    'plusmn', 'sup2',   'sup3',   'acute',  'micro',  'para',   'middot', 'cedil',  'sup1',   'ordm',   'raquo',  'frac14', 'frac12', 'frac34', 'iquest', 'Agrave', 'Aacute', 'Acirc',  'Atilde', 'Auml',   'Aring',  'AElig',  'Ccedil','Egrave','Eacute','Ecirc', 'Euml',  'Igrave', 'Iacute','Icirc', 'Iuml',  'ETH',   'Ntilde','Ograve','Oacute','Ocirc', 'Otilde','Ouml',  'times', 'Oslash','Ugrave','Uacute','Ucirc', 'Uuml',  'Yacute','THORN', 'szlig', 'agrave','aacute','acirc', 'atilde','auml',  'aring', 'aelig', 'ccedil','egrave','eacute','ecirc', 'euml',  'igrave', 'iacute','icirc', 'iuml',  'eth',   'ntilde','ograve', 'oacute','ocirc', 'otilde','ouml',  'divide','oslash','ugrave','uacute','ucirc', 'uuml',  'yacute','thorn', 'yuml']
                 
     def process_document(self, session, doc):
