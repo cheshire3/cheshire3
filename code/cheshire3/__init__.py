eaa66e6c480b157305666e66c317bf25ba2db144
Robert Sanderson <azaroth@liv.ac.uk>
2008-08-14 15:11:30 +0000

    no more array stuff
    git-svn-id: http://cvs.cheshire3.org/repos/cheshire3/cheshire3@952 04eb1732-f344-4bcc-95d5-7f11b6d85e6b

--- code/cheshire3/__init__.py
+++ code/cheshire3/__init__.py
@@ -1,10 +1,52 @@
 
+
+# --- require print() and make all strings unicodes
 #from __future__ import print_function
 #from __future__ import unicode_literals
 
+import sys, os
+
 # Tests for Python 3.0 incompatibility
-#import sys
 #sys.py3kwarning = True
+
+
+# Hack to allow us to patch a module in a different directory
+import __builtin__
+
+home = os.environ.get("C3HOME")
+if home:
+    C3MODULEPATH=os.path.join(home, "cheshire3", "code")
+else:
+    C3MODULEPATH = '/home/cheshire/cheshire3/code'
+builtin_importer = __builtin__.__import__
+
+def patch(enable=True):
+
+    if enable:
+        def import_module(name, globals = None, locals = None, fromlist = None, *args, **kw):
+
+            if name[:10] == "cheshire3.":
+                # try to import from code first
+                sys.path.insert(1, C3MODULEPATH)
+                try:
+                    oname = "c3patch.%s" % name[10:]            
+                    m =  builtin_importer(oname, globals,locals, fromlist)
+                    return m
+                except ImportError:
+                    sys.path.pop(1)
+                    m = builtin_importer(name, globals, locals, fromlist)
+                    return m
+                sys.path.pop(1)
+                return m            
+            else:
+                return builtin_importer(name, globals, locals, fromlist)
+
+        __builtin__.__import__ = import_module
+    else:
+        __builtin__.__import__ = builtin_importer
+
+
+
 
 __name__ = "cheshire3"
 __package__ = "cheshire3"
