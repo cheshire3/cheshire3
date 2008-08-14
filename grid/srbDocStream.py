eca3d480d27d55a50ee68b5af06d63c5d26c28d5
Robert Sanderson <azaroth42@gmail.com>
2008-08-14 09:42:47 +0000

    MODULARIZATION HAPPENS HERE, EVERYTHING BREAKS...
    git-svn-id: http://cvs.cheshire3.org/repos/cheshire3/cheshire3/code@945 04eb1732-f344-4bcc-95d5-7f11b6d85e6b

--- grid/srbDocStream.py
+++
@@ -1,9 +0,0 @@
-
-
-from documentFactory import RemoteDocumentStream, MultipleDocumentStream
-
-# XXX Should go to grid package
-class SrbDocumentStream(RemoteDocumentStream, MultipleDocumentStream):
-    # SRB://user.domain:pass@host:port/path/to/object?DEFAULTRESOURCE=res
-    pass
-
--- textmining/tmExtractor.py
+++
@@ -1,2 +0,0 @@
-
-from extractor import SimpleExtractor
