637d9078fd59cbcb38e9d637a83bb708a339dfd8
John Harrison <john.harrison@liv.ac.uk>
2008-09-17 15:05:48 +0000

    bug fixes to FieldStorageQueryStream
    git-svn-id: http://cvs.cheshire3.org/repos/cheshire3/cheshire3@1015 04eb1732-f344-4bcc-95d5-7f11b6d85e6b

--- code/cheshire3/queryFactory.py
+++ code/cheshire3/queryFactory.py
@@ -13,61 +13,69 @@ class CqlQueryStream(QueryStream):
         # XXX check codec, turn into unicode first
         return cql.parse(data)
 
-
-class FieldStorageQueryStream(QueryStream):
-    
-    def __init__(self):
-        QueryStream.__init__(self)
-        self.phraseRe = re.compile('".+?"')
-    
-    def parse(self, session, data, codec, db):
-        qClauses = []
-        bools = []
-        i = 1
-        while (form.has_key('fieldcont%d' % i)):
-            bools.append(form.getfirst('fieldbool%d' % (i-1), 'and/relevant/proxinfo'))
-            i += 1
-            
-        i = 1
-        while (form.has_key('fieldcont%d' % i)):
-            cont = form.getfirst('fieldcont%d' % i)
-            idxs = cgi_decode(form.getfirst('fieldidx%d' % i, 'cql.anywhere'))
-            rel = cgi_decode(form.getfirst('fieldrel%d'  % i, 'all/relevant/proxinfo'))
-            idxClauses = []
-            for idx in idxs.split('||'):
-                subClauses = []
-                if (rel[:3] == 'all'): subBool = ' and/relevant/proxinfo '
-                else: subBool = ' or/relevant/proxinfo '
+try:
+    from cheshire3.web.www_utils import cgi_decode
+except:
+    class FieldStorageQueryStream(QueryStream):
+        def __init__(self):
+            raise NotImplementedError('Requires cheshire3.web sub-package')
         
-                # in case they're trying to do phrase searching
-                if (rel.find('exact') != -1 or rel.find('=') != -1 or rel.find('/string') != -1):
-                    # don't allow phrase searching for exact or /string searches
-                    cont = cont.replace('"', '\\"')
-                else:
-                    phrases = self.phraseRe.findall(cont)
-                    for ph in phrases:
-                        subClauses.append('(%s =/relevant/proxinfo %s)' % (idx, ph))
-                    
-                    cont = self.phraseRe.sub('', cont)
-                         
-                if (idx and rel and cont):
-                    subClauses.append('%s %s "%s"' % (idx, rel, cont.strip()))
-                    
-                if (len(subClauses)):
-                    idxClauses.append('(%s)' % (subBool.join(subClauses)))
+else:
+    class FieldStorageQueryStream(QueryStream):
+        
+        def __init__(self):
+            QueryStream.__init__(self)
+            self.phraseRe = re.compile('".+?"')
+        
+        def parse(self, session, data, codec, db):
+            form = data
+            qClauses = []
+            bools = []
+            i = 1
+            while (form.has_key('fieldcont%d' % i)):
+                bools.append(form.getfirst('fieldbool%d' % (i-1), 'and/relevant/proxinfo'))
+                i += 1
                 
-            qClauses.append('(%s)' % (' or/relevant/proxinfo '.join(idxClauses)))
-            # if there's another clause and a corresponcding boolean
-            try: qClauses.append(bools[i])
-            except: break
+            i = 1
+            while (form.has_key('fieldcont%d' % i)):
+                cont = form.getfirst('fieldcont%d' % i)
+                idxs = cgi_decode(form.getfirst('fieldidx%d' % i, 'cql.anywhere'))
+                rel = cgi_decode(form.getfirst('fieldrel%d'  % i, 'all/relevant/proxinfo'))
+                idxClauses = []
+                for idx in idxs.split('||'):
+                    subClauses = []
+                    if (rel[:3] == 'all'): subBool = ' and/relevant/proxinfo '
+                    else: subBool = ' or/relevant/proxinfo '
             
-            i += 1
-            
-        qString = ' '.join(qClauses)
-        formcodec = form.getfirst('_charset_', 'utf-8')
-        return cql.parse(qString.decode(formcodec).encode('utf-8'))
-
-    #- end FieldStorageQueryStream() ------------------------------------------
+                    # in case they're trying to do phrase searching
+                    if (rel.find('exact') != -1 or rel.find('=') != -1 or rel.find('/string') != -1):
+                        # don't allow phrase searching for exact or /string searches
+                        cont = cont.replace('"', '\\"')
+                    else:
+                        phrases = self.phraseRe.findall(cont)
+                        for ph in phrases:
+                            subClauses.append('(%s =/relevant/proxinfo %s)' % (idx, ph))
+                        
+                        cont = self.phraseRe.sub('', cont)
+                             
+                    if (idx and rel and cont):
+                        subClauses.append('%s %s "%s"' % (idx, rel, cont.strip()))
+                        
+                    if (len(subClauses)):
+                        idxClauses.append('(%s)' % (subBool.join(subClauses)))
+                    
+                qClauses.append('(%s)' % (' or/relevant/proxinfo '.join(idxClauses)))
+                # if there's another clause and a corresponcding boolean
+                try: qClauses.append(bools[i])
+                except: break
+                
+                i += 1
+                
+            qString = ' '.join(qClauses)
+            formcodec = form.getfirst('_charset_', 'utf-8')
+            return cql.parse(qString.decode(formcodec).encode('utf-8'))
+    
+        #- end FieldStorageQueryStream() ------------------------------------------
     
 
 class SimpleQueryFactory(QueryFactory):
