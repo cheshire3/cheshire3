6e04e85dafee7fc2f4163fa6fe64bfdb99aea411
Robert Sanderson <azaroth42@gmail.com>
2009-03-27 17:08:19 +0000

    implemented transformer
    git-svn-id: http://cvs.cheshire3.org/repos/cheshire3/cheshire3/code@1561 04eb1732-f344-4bcc-95d5-7f11b6d85e6b

--- cheshire3/graph/transformer.py
+++ cheshire3/graph/transformer.py
@@ -1,5 +1,17 @@
 
 from cheshire3.transformer import Transformer
+from cheshire3.graph.record import GraphRecord
+from cheshire3.document import StringDocument
 
-class RdfTransformer(Transformer):
-    pass
+class RdfXmlTransformer(Transformer):
+    # take GraphRecord and serialize to doc
+
+    _possibleSettings = {'format' : {'docs' : 'format to serialize to, default to simple xml.'}}
+
+    def process_record(session, rec):
+        if isinstance(rec, GraphRecord):
+            fmt = self.get_setting(session, 'format', 'xml')
+            data = rec.graph.serialize(format=fmt)
+            return StringDOcument(session, data)
+        else:
+            raise NotImplementedError("Can only transform GraphRecords")
