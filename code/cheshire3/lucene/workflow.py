d5f769803dc415858900ccc8d180f1893186b282
Robert Sanderson <azaroth@liv.ac.uk>
2009-04-01 17:00:33 +0000

    indexStore interoperability: both ingest and search
    git-svn-id: http://cvs.cheshire3.org/repos/cheshire3/cheshire3@1573 04eb1732-f344-4bcc-95d5-7f11b6d85e6b

--- code/cheshire3/lucene/workflow.py
+++ code/cheshire3/lucene/workflow.py
@@ -8,11 +8,14 @@ import lucene
 
 class AnalyzerWorkflow(SimpleWorkflow):
 
+    _possibleSettings = {'analyzer' : {'docs' : 'An Analyzer wrapped as a workflow'}}
+
     def __init__(self, session, config, parent):
         SimpleWorkflow.__init__(self, session, config, parent)
         anlr = self.get_setting(session, 'analyzer')
         if anlr[-8:] == 'Analyzer' and hasattr(lucene, anlr):            
-            self.analyzer = getattr(lucene, anlr)
+            anlzClass = getattr(lucene, anlr)
+            self.analyzer = anlzClass()
         else:
             raise ConfigFileException("Unknown Lucene Analyzer")
         pass
@@ -21,14 +24,27 @@ class AnalyzerWorkflow(SimpleWorkflow):
         # input should be:
         #   {txt : {'text' : txt, 'occurences' : 1, 'proxLoc' : []}}
         # only thing we can push through is txt
+        # output is as from tokenizer (eg to be merged)
 
+        if not data:
+            return data
+        
+        kw = {}
+        first = data.popitem()
+        prox = first[1].has_key('proxLoc')
+        data[first[0]] = first[1]
         if type(data) == dict:
             for k in data.keys():                
                 rdr = lucene.StringReader(data[k]['text'])        
-                res = self.analyzer(rdr)
-                toks = [t.term() for t in res]
-                print repr(toks)
+                res = self.analyzer.tokenStream('data', rdr)
 
+                # can also get offset information from terms
+                toks = [t.term() for t in res]
+
+                kw[k] = {'text' :toks, 'occurences' : 1}
+                if prox:
+                    kw[k]['proxLoc'] = data[k]['proxLoc']
+        return kw
 
 
 ### Available Analyzers:
