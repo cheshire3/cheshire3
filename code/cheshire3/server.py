ef0fce1220a88637c6b02949375f6261b84a7c4e
Robert Sanderson <azaroth@liv.ac.uk>
2009-04-06 15:44:56 +0000

    removing pointless comment from server.py
    git-svn-id: http://cvs.cheshire3.org/repos/cheshire3/cheshire3@1586 04eb1732-f344-4bcc-95d5-7f11b6d85e6b

--- code/cheshire3/server.py
+++ code/cheshire3/server.py
@@ -21,7 +21,6 @@ class SimpleServer(Server):
         except:
             topNode = dom
         C3Object.__init__(self, session, topNode, None)
-        # and here is a new line
 
     def _cacheDatabases(self, session):
         for dbid in self.databaseConfigs.keys():
