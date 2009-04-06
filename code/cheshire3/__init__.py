9f1a75eac6ab78aab0cbe6ef41f755abfb84425c
Robert Sanderson <azaroth@liv.ac.uk>
2009-04-06 14:08:57 +0000

    removing patch hack
    git-svn-id: http://cvs.cheshire3.org/repos/cheshire3/cheshire3@1584 04eb1732-f344-4bcc-95d5-7f11b6d85e6b

--- code/cheshire3/__init__.py
+++ code/cheshire3/__init__.py
@@ -13,43 +13,7 @@ import sys, os
 from warnings import filterwarnings
 filterwarnings('ignore', 'the md5 module is deprecated; use hashlib instead',  DeprecationWarning, 'yacc')
 
-# Hack to allow us to patch a module in a different directory
-import __builtin__
-
 home = os.environ.get("C3HOME")
-if home:
-    C3MODULEPATH=os.path.join(home, "cheshire3", "code")
-else:
-    C3MODULEPATH = '/home/cheshire/cheshire3/code'
-builtin_importer = __builtin__.__import__
-
-def patch(enable=True):
-
-    if enable:
-        def import_module(name, globals = None, locals = None, fromlist = None, *args, **kw):
-
-            if name[:10] == "cheshire3.":
-                # try to import from code first
-                sys.path.insert(1, C3MODULEPATH)
-                try:
-                    oname = "c3patch.%s" % name[10:]            
-                    m =  builtin_importer(oname, globals,locals, fromlist)
-                    return m
-                except ImportError:
-                    sys.path.pop(1)
-                    m = builtin_importer(name, globals, locals, fromlist)
-                    return m
-                sys.path.pop(1)
-                return m            
-            else:
-                return builtin_importer(name, globals, locals, fromlist)
-
-        __builtin__.__import__ = import_module
-    else:
-        __builtin__.__import__ = builtin_importer
-
-
-
 
 __name__ = "cheshire3"
 __package__ = "cheshire3"
@@ -66,6 +30,7 @@ import cheshire3.internal
 sps = cheshire3.internal.get_subpackages()
 
 sps= ['web']
+
 for sp in sps:
     # call import for on init hooks
     try:
