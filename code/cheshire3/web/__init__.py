16f0fc450fff2ac9245c4f09b60abcf937fa81ec
John Harrison <john.harrison@liv.ac.uk>
2009-06-19 13:41:44 +0000

    +func, style, docs AccHttpDocumentStream added for use with AccumulatingDocumentFactory call load() on many URLs to accumulate documents e.g. for web-crawling www.__init__ registers AccHttpDocumentStream with AccumulatingDocumentFactory via classmethod existing AccumulatingDocumentFactory stream registrations made classmethod rather than instance some better python docstrings
    git-svn-id: http://cvs.cheshire3.org/repos/cheshire3/cheshire3@1915 04eb1732-f344-4bcc-95d5-7f11b6d85e6b

--- code/cheshire3/web/__init__.py
+++ code/cheshire3/web/__init__.py
@@ -7,7 +7,10 @@ import sys
 
 # Register our streams with main docFac
 # register_stream is an @classmethod
-from cheshire3.web.documentFactory import streamHash
-from cheshire3.documentFactory import SimpleDocumentFactory
+from cheshire3.web.documentFactory import streamHash, accStreamHash
+from cheshire3.documentFactory import SimpleDocumentFactory, AccumulatingDocumentFactory
 for (k,v) in streamHash.items():
     SimpleDocumentFactory.register_stream(k, v)
+    
+for (k,v) in accStreamHash.items():
+    AccumulatingDocumentFactory.register_stream(k, v)
