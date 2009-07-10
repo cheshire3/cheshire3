b14c988d22ab55de3b7e319b893669249ed5d12c
Robert Sanderson <azaroth@liv.ac.uk>
2009-07-10 14:06:36 +0000

    +func: GraphRecord implementation for get_xml() get_dom() and make process_xpath() work for xpaths (and fall back to sparql on failure
    git-svn-id: http://cvs.cheshire3.org/repos/cheshire3/cheshire3@2067 04eb1732-f344-4bcc-95d5-7f11b6d85e6b

--- code/cheshire3/graph/record.py
+++ code/cheshire3/graph/record.py
@@ -2,7 +2,7 @@
 from cheshire3.record import LxmlRecord
 from cheshire3.baseObjects import Record
 
-class GraphRecord(Record):
+class GraphRecord(LxmlRecord):
     graph = None
 
     def __init__(self, session, data, xml="", docId=None, wordCount=0, byteCount=0):
@@ -12,13 +12,32 @@ class GraphRecord(Record):
         self.parent = ('','',-1)
         self.context = None
 
-    def process_xpath(self, session, q, map={}):
-        return self.process_sparql(session, q, map)
-
     def process_sparql(self, session, q, map={}):
         return self.graph.query(q, initNs=map)
 
     def get_graph(self, session):
         return self.graph
 
-    
+    def process_xpath(self, session, q, map={}):
+        if not self.dom:
+            self.get_xml(session)
+        try:
+            return LxmlRecord.process_xpath(self, session, q, map)
+        except:
+            return self.process_sparql(session, q, map)
+            
+    def get_xml(self, session):
+        data = self.graph.serialize(format='pretty-xml')
+        if not self.dom:
+            try:
+                et = etree.XML(data)
+            except AssertionError:
+                data = data.decode('utf8')
+                et = etree.XML(data)            
+            self.dom = et
+        return data
+
+    def get_dom(self, session):
+        if not self.dom:
+            self.get_xml(session)
+        return self.dom
