e9c9f588902ae94eaf0d5cf7bf569952e04c9006
Catherine Smith <cjsmith@liv.ac.uk>
2009-12-14 14:16:32 +0000

    docs changed init.py
    git-svn-id: http://cvs.cheshire3.org/repos/cheshire3/cheshire3@2490 04eb1732-f344-4bcc-95d5-7f11b6d85e6b

--- code/cheshire3/formats/__init__.py
+++ code/cheshire3/formats/__init__.py
@@ -1,4 +1,7 @@
 __name__ = "Cheshire3 File Formats Package"
 __package__ = "formats"
 
-__all__ = ['preParser']
+__all__ = ['preParser', 'documentFactory']
+
+import cheshire3.formats.documentFactory
+import cheshire3.formats.preParser
