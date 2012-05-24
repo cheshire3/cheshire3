65dae43a6de887755ab7cb8a45d5fe7d68b87887
John Harrison <john.harrison@liv.ac.uk>
2012-05-24 16:33:52 +0100

    added cheshire3_search to cheshire3.commands.__all__

--- code/cheshire3/commands/__init__.py
+++ code/cheshire3/commands/__init__.py
@@ -1,3 +1,3 @@
 """Command-line User Interfaces for Cheshire3."""
 
-__all__ = ['cmd_utils', 'cheshire3_console', 'cheshire3_init', 'cheshire3_load']
+__all__ = ['cmd_utils', 'cheshire3_console', 'cheshire3_init', 'cheshire3_load', 'cheshire3_search']
