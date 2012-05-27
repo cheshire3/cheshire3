3369689467c3e556b475ea4b942d954154f9bc10
John Harrison <john@bloomonkey.co.uk>
2012-05-27 22:27:43 +0100

    added some unittests for cheshire3.documentFactory

---
+++ code/cheshire3/test/testDocumentFactory.py
@@ -0,0 +1,87 @@
+u"""Cheshire3 DocumentFactory Unittests.
+
+DocumentFactory configurations may be customized by the user. For the purposes 
+of unittesting, configuration files will be ignored and DocumentFactory 
+instances will be instantiated using configuration data defined within this 
+testing module, and tests carried out on instances.
+"""
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+    
+import string
+
+from lxml import etree
+
+from cheshire3.documentFactory import SimpleDocumentFactory, \
+ComponentDocumentFactory
+from cheshire3.test.testConfigParser import Cheshire3ObjectTestCase
+
+
+class DocumentFactoryTestCase(Cheshire3ObjectTestCase):
+    """Base Class for Cheshire3 DocumentFactory Test Case."""
+    
+    def _get_class(self):
+        return SimpleDocumentFactory
+    
+    def _get_config(self):
+        return etree.XML('''\
+<subConfig type="documentFactory" id="baseDocumentFactory">
+    <objectType>cheshire3.documentFactory.{0}</objectType>
+</subConfig>'''.format(self._get_class().__name__))
+        
+    def _get_testData(self):
+        return ['<doc/>',
+                '<doc><foo/><bar/><baz/></doc>']
+        
+    def test_instance(self):
+        self.assertIsInstance(self.testObj, self._get_class())
+    
+    def test_load(self):
+        # Test that load method of an instance returns the instance
+        for data in self._get_testData():
+            thing = self.testObj.load(self.session, data)
+            self.assertIsInstance(thing, self._get_class())
+        
+
+class ComponentDocumentFactoryTestCase(DocumentFactoryTestCase):
+    """Simple ComponentDocumentFactory Test Case."""
+    
+    def _get_class(self):
+        return ComponentDocumentFactory
+    
+    def _get_config(self):
+        # Return a parsed config for the object to be tested
+        return etree.XML('''\
+<subConfig type="documentFactory" id="componentDocumentFactory">
+    <objectType>cheshire3.documentFactory.{0}</objectType>
+    <source>
+      <xpath>bar</xpath>
+      <xpath>baz</xpath>
+    </source>
+    <options>
+        <default type="cache">0</default>
+        <default type="format">component</default>
+    </options>
+</subConfig>'''.format(self._get_class().__name__))
+    
+
+class SpanComponentDocumentFactoryTestCase(ComponentDocumentFactoryTestCase):
+    """Span ComponentDocumentFactory Test Case."""
+    
+    def _get_config(self):
+        # Return a parsed config for the object to be tested
+        return etree.XML('''\
+        ''')
+
+
+def load_tests(loader, tests, pattern):
+    suite = loader.loadTestsFromTestCase(ComponentDocumentFactoryTestCase)
+    return suite
+
+
+if __name__ == '__main__':
+    tr = unittest.TextTestRunner(verbosity=2)
+    tr.run(load_tests(unittest.defaultTestLoader, [], 'test*.py'))
