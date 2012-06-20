d8c1807da7cc7381c45c4b139bf46978d0522734
John Harrison <john.harrison@liv.ac.uk>
2012-06-20 16:01:48 +0100

    made cheshire3.protocolMap PEP8 compliant

--- code/cheshire3/protocolMap.py
+++ code/cheshire3/protocolMap.py
@@ -1,8 +1,16 @@
-# Map incoming request stuff into local objects
-# Eg dc.title -> l5r-idx-cardname object
+"""ProtocolMap implemetations
 
-from cheshire3.baseObjects import ProtocolMap, Extractor
-from cheshire3.exceptions import ConfigFileException, ObjectDoesNotExistException
+A ProtocolMap maps incoming requests into local objects in order to handle
+those requests.
+e.g. dc.title -> l5r-idx-cardname object
+"""
+
+import sys
+import os
+
+from cheshire3.baseObjects import ProtocolMap
+from cheshire3.exceptions import ConfigFileException,\
+                                 ObjectDoesNotExistException
 from cheshire3.utils import elementType, textType, flattenTexts
 from cheshire3.cqlParser import modifierClauseType, indexType, relationType
 from cheshire3 import dynamic
@@ -12,23 +20,26 @@ try:
 except:
     srwExtensions = None
 
-import sys, os
 
 class ZeerexProtocolMap(ProtocolMap):
+    """Abstract Base Class for ProtocolMaps based on the ZeeRex specification.
+    
+    http://zeerex.z3950.org/
+    """
     protocol = ""
     version = ""
     c3Namespace = "http://www.cheshire3.org/schemas/explain/"
 
-
-    _possiblePaths = {'zeerexPath' : {'docs' : "Path to ZeeRex file."}}
+    _possiblePaths = {'zeerexPath': {'docs': "Path to ZeeRex file."}}
 
     def __init__(self, session, node, parent):
         ProtocolMap.__init__(self, session, node, parent)
         p = self.get_path(session, 'zeerexPath')
         if (p is None):
-            raise(ConfigFileException('ZeerexPath not specified for CQLConfig.'))
+            raise ConfigFileException('ZeerexPath not specified for '
+                                      'CQLConfig.')
         else:
-            if ( not os.path.isabs(p)):
+            if not os.path.isabs(p):
                 dfp = self.get_path(session, 'defaultPath')
                 p = os.path.join(dfp, p)
             dom = self._getDomFromFile(session, p, parser='minidom')
@@ -36,7 +47,13 @@ class ZeerexProtocolMap(ProtocolMap):
             if c.nodeType == elementType:
                 self._walkZeeRex(session, c)    
 
+
 class UpdateProtocolMap(ZeerexProtocolMap):
+    """ProtocolMap for the SRU Record Update protocol.
+    
+    http://www.loc.gov/standards/sru/record-update/index.html
+    """
+    
     transformerHash = {}
     workflowHash = {}
 
@@ -45,14 +62,11 @@ class UpdateProtocolMap(ZeerexProtocolMap):
         self.protocolNamespaces = protocolNamespaces
         self.recordNamespaces = recordNamespaces
         self.extensionNamespaces = extensionNamespaces
-
         self.transformerHash = {}
         self.workflowHash = {}
-
         ZeerexProtocolMap.__init__(self, session, node, parent)
 
     def _walkZeeRex(self, session, node):
-
         if node.localName in ['databaseInfo', 'metaInfo', 'indexInfo']:
             # Ignore
             return
@@ -73,7 +87,8 @@ class UpdateProtocolMap(ZeerexProtocolMap):
             if (xsl):
                 txr = self.get_object(session, xsl)
                 if (txr is None):
-                    raise ConfigFileException("No transformer to map to for %s" % (xsl))
+                    raise ConfigFileException("No transformer to map to for "
+                                              "%s" % (xsl))
                 self.transformerHash[id] = txr
             self.recordNamespaces[name] = id
         elif node.localName == "supports":
@@ -84,7 +99,8 @@ class UpdateProtocolMap(ZeerexProtocolMap):
                 if (wflw):
                     flow = self.get_object(session, wflw)
                     if (flow is None):                        
-                        raise ConfigFileException("No workflow to map to for %s" % wflw)
+                        raise ConfigFileException("No workflow to map to for "
+                                                  "%s" % wflw)
                     self.workflowHash[data] = self.get_object(session, wflw)
         elif (node.localName == 'default'):
             dtype = node.getAttribute('type')
@@ -97,7 +113,7 @@ class UpdateProtocolMap(ZeerexProtocolMap):
             elif data == 'true':
                 data = 1
             setattr(self, pname, data)
-        elif (node.localName =='setting'):
+        elif (node.localName == 'setting'):
             dtype = node.getAttribute('type')
             data = flattenTexts(node)
             if (data.isdigit()):
@@ -114,6 +130,10 @@ class UpdateProtocolMap(ZeerexProtocolMap):
                     
 
 class CQLProtocolMap(ZeerexProtocolMap):
+    """ProtocolMap for the Contextual Query Language.
+    
+    http://www.loc.gov/standards/sru/specs/cql.html
+    """
     
     prefixes = {}
     indexHash = {}
@@ -124,7 +144,6 @@ class CQLProtocolMap(ZeerexProtocolMap):
     scanExtensionHash = {}
     explainExtensionHash = {}
     responseExtensionHash = {}
-
     
     def __init__(self, session, node, parent):
         self.protocol = "http://www.loc.gov/zing/srw/"
@@ -147,14 +166,14 @@ class CQLProtocolMap(ZeerexProtocolMap):
         self.extensionTypeMap = {}
         ZeerexProtocolMap.__init__(self, session, node, parent)
 
-
     def resolvePrefix(self, name):
         if (name in self.prefixes):
             return self.prefixes[name]
         elif not name:
             # Look for default
             if not hasattr(self, 'defaultContextSet'):
-                raise ConfigFileException('Zeerex does not have default context set.')
+                raise ConfigFileException('Zeerex does not have default '
+                                          'context set.')
             default = self.defaultContextSet
             if (default in self.prefixes):
                 return self.prefixes[default]
@@ -185,7 +204,10 @@ class CQLProtocolMap(ZeerexProtocolMap):
                 val = query.index.origValue.split('.')[1]
                 idx = self.parent.get_object(session, val)
             return idx
-        elif uri in ['info:srw/cql-context-set/1/cql-v1.1', 'info:srw/cql-context-set/1/cql-v1.2'] and name == 'serverchoice' and hasattr(self, 'defaultIndex'):
+        elif (uri in ['info:srw/cql-context-set/1/cql-v1.1',
+                     'info:srw/cql-context-set/1/cql-v1.2'] and
+              name == 'serverchoice' and
+              hasattr(self, 'defaultIndex')):
             dp, dn = self.defaultIndex.split('.')
             du = self.resolvePrefix(dp)
             query.index.prefix = dp
@@ -213,23 +235,27 @@ class CQLProtocolMap(ZeerexProtocolMap):
         
         idx = None
         if (relv):
-            idx = self.indexHash.get((uri, name, ('relationModifier', 'relevant')), None)
+            idx = self.indexHash.get((uri, name,
+                                      ('relationModifier', 'relevant')),
+                                     None)
         if (not idx and stem):
-            idx = self.indexHash.get((uri, name, ('relationModifier', 'stem')), None)
+            idx = self.indexHash.get((uri, name,
+                                      ('relationModifier', 'stem')),
+                                     None)
         if (not idx and rms):
             for rm in rms:
-                idx = self.indexHash.get((uri, name, ('relationModifier', rm)), None)
+                idx = self.indexHash.get((uri, name,
+                                          ('relationModifier', rm)),
+                                         None)
                 if (idx):
                     break
         if (not idx):
             idx = self.indexHash.get((uri, name, ('relation', rel)), None)
         if (not idx):
-            idx = self.indexHash.get((uri,name), None)
+            idx = self.indexHash.get((uri, name), None)
         return idx
 
-
     def _walkZeeRex(self, session, node):
-
         if node.localName in ['databaseInfo', 'metaInfo']:
             # Ignore
             return
@@ -250,21 +276,24 @@ class CQLProtocolMap(ZeerexProtocolMap):
             if (xsl):
                 txr = self.get_object(session, xsl)
                 if (txr is None):
-                    raise ConfigFileException("No transformer to map to for %s" % (xsl))
+                    raise ConfigFileException("No transformer to map to for "
+                                              "%s" % (xsl))
                 self.transformerHash[id] = txr
             self.recordNamespaces[name] = id
         elif node.localName == 'set':
             name = node.getAttribute('name')
             uri = node.getAttribute('identifier')
             if (name in self.prefixes and uri != self.prefixes[name]):
-                raise(ConfigFileException('Multiple URIs bound to same short name: %s -> %s' % (name, uri)))
+                raise ConfigFileException('Multiple URIs bound to same short '
+                                          'name: %s -> %s' % (name, uri))
             self.prefixes[str(name)] = str(uri)
         elif node.localName == 'index':
             # Process indexes
             idxName = node.getAttributeNS(self.c3Namespace, 'index')
             indexObject = self.get_object(session, idxName)
             if indexObject is None:
-                raise(ConfigFileException("[%s] No Index to map to for %s" % (self.id, idxName)))
+                raise(ConfigFileException("[%s] No Index to map to for "
+                                          "%s" % (self.id, idxName)))
             maps = []
 
             for c in node.childNodes:
@@ -276,18 +305,26 @@ class CQLProtocolMap(ZeerexProtocolMap):
             for c in node.childNodes:
                 if (c.nodeType == elementType and c.localName == 'configInfo'):
                     for c2 in c.childNodes:
-                        if (c2.nodeType == elementType and c2.localName == 'supports'):
-                            idxName2 = c2.getAttributeNS(self.c3Namespace, 'index')
+                        if (c2.nodeType == elementType and
+                            c2.localName == 'supports'):
+                            idxName2 = c2.getAttributeNS(self.c3Namespace,
+                                                         'index')
                             if (not idxName2):
                                 indexObject2 = indexObject
                             else:
-                                indexObject2 = self.get_object(session, idxName2)
+                                indexObject2 = self.get_object(session,
+                                                               idxName2)
                                 if indexObject2 is None:
-                                    raise(ConfigFileException("[%s] No Index to map to for %s" % (self.id, idxName2)))
+                                    raise ConfigFileException(
+                                              "[%s] No Index to map to for "
+                                              "%s" % (self.id, idxName2)
+                                    )
                             st = str(c2.getAttribute('type'))
                             val = str(flattenTexts(c2))
                             for m in maps:
-                                self.indexHash[(m[0], m[1], (st, val))] = indexObject2
+                                self.indexHash[(m[0],
+                                                m[1],
+                                                (st, val))] = indexObject2
 
         elif (node.localName == 'map'):
             for c in node.childNodes:
@@ -297,11 +334,13 @@ class CQLProtocolMap(ZeerexProtocolMap):
                     index = index.lower()
                     uri = self.resolvePrefix(short)
                     if (not uri):
-                        raise(ConfigFileException("No mapping for %s in Zeerex" % (short)))
+                        raise ConfigFileException("No mapping for %s in "
+                                                  "Zeerex" % (short))
                     return (str(uri), str(index))
         elif (node.localName == 'default'):
             dtype = node.getAttribute('type')
-            pname = "default" + dtype[0].capitalize() + dtype[1:] # XXX: would dtype.title() be nicer!?
+            # XXX: would dtype.title() be nicer!?
+            pname = "default" + dtype[0].capitalize() + dtype[1:]
             data = flattenTexts(node)
             if (data.isdigit()):
                 data = int(data)
@@ -310,7 +349,7 @@ class CQLProtocolMap(ZeerexProtocolMap):
             elif data == 'true':
                 data = 1
             setattr(self, pname, data)
-        elif (node.localName =='setting'):
+        elif (node.localName == 'setting'):
             dtype = node.getAttribute('type')
             data = flattenTexts(node)
             if (data.isdigit()):
@@ -322,12 +361,14 @@ class CQLProtocolMap(ZeerexProtocolMap):
             setattr(self, dtype, data)
         elif (node.localName == 'supports'):
             stype = node.getAttribute('type')
-            if stype in ['extraData', 'extraSearchData', 'extraScanData', 'extraExplainData', 'extension']:
+            if stype in ['extraData', 'extraSearchData', 'extraScanData',
+                         'extraExplainData', 'extension']:
                 # function, xnType, sruName
 
                 xn = node.getAttributeNS(self.c3Namespace, 'type')
-                if (not xn in ['record', 'term', 'searchRetrieve', 'scan', 'explain', 'response']):
-                    raise (ConfigFileException('Unknown extension type %s' % xn))               
+                if (not xn in ['record', 'term', 'searchRetrieve', 'scan',
+                               'explain', 'response']):
+                    raise ConfigFileException('Unknown extension type %s' % xn)               
                 sru = node.getAttributeNS(self.c3Namespace, 'sruName')
                 fn = node.getAttributeNS(self.c3Namespace, 'function')
                 data = flattenTexts(node)
@@ -339,20 +380,27 @@ class CQLProtocolMap(ZeerexProtocolMap):
                     try:
                         fn = dynamic.importObject(session, fn)
                     except ImportError:
-                        raise ConfigFileException("Cannot find handler function %s (in %s)" % (fn, repr(sys.path)))
+                        raise ConfigFileException("Cannot find handler "
+                                                  "function %s (in %s)" %
+                                                  (fn, repr(sys.path)))
                     self.sruExtensionMap[sru] = (xn, fn, data)
                 else:
                     if (hasattr(srwExtensions, fn)):
                         fn = getattr(srwExtensions, fn)
                     else:
-                        raise(ConfigFileException('Cannot find handler function %s in srwExtensions.' % fn))
-                    xform = node.getAttributeNS(self.c3Namespace, 'sruFunction')
+                        raise ConfigFileException('Cannot find handler '
+                                                  'function %s in '
+                                                  'srwExtensions.' % fn)
+                    xform = node.getAttributeNS(self.c3Namespace,
+                                                'sruFunction')
                     if not xform:
                         sruFunction = srwExtensions.simpleRequestXform
                     elif hasattr(srwExtensions, xform):
                         sruFunction = getattr(srwExtensions, xform)
                     else:
-                        raise(ConfigFileException('Cannot find transformation function %s in srwExtensions.' % xform))
+                        raise ConfigFileException('Cannot find transformation '
+                                                  'function %s in '
+                                                  'srwExtensions.' % xform)
                     hashAttr = xn + "ExtensionHash"                
                     curr = getattr(self, hashAttr)
                     curr[data] = fn
@@ -362,7 +410,6 @@ class CQLProtocolMap(ZeerexProtocolMap):
             for c in node.childNodes:
                 if c.nodeType == elementType:
                     self._walkZeeRex(session, c)
-                    
 
     def parseExtraData(self, extra):
         nodes = []
@@ -375,49 +422,49 @@ class CQLProtocolMap(ZeerexProtocolMap):
 
 
 protocolNamespaces = {
-  'srw' : 'http://www.loc.gov/zing/srw/',
-  'xcql' : 'http://www.loc.gov/zing/srw/xcql/',
-  'diag' : 'http://www.loc.gov/zing/srw/diagnostic/',
-  'ucp' : 'http://www.loc.gov/zing/srw/update/'
+  'srw': 'http://www.loc.gov/zing/srw/',
+  'xcql': 'http://www.loc.gov/zing/srw/xcql/',
+  'diag': 'http://www.loc.gov/zing/srw/diagnostic/',
+  'ucp': 'http://www.loc.gov/zing/srw/update/'
 }
 
 recordNamespaces = {
-  'dc' : 'info:srw/schema/1/dc-v1.1',
-  'diag' : 'info:srw/schema/1/diagnostic-v1.1',
-  'mods' : 'info:srw/schema/1/mods-v3.0',
-  'onix' : 'info:srw/schema/1/onix-v2.0',
-  'marcxml' : 'info:srw/schema/1/marcxml-v1.1',
-  'ead' : 'info:srw/schema/1/ead-2002',
-  'ccg' : 'http://srw.o-r-g.org/schemas/ccg/1.0/',
-  'marcsgml' : 'http://srw.o-r-g.org/schemas/marcsgml/12.0/',
-  'metar' : 'http://srw.o-r-g.org/schemas/metar/1.0/',
-  'unesco' : 'http://srw.o-r-g.org/schemas/unesco/1.0/',
-  'zthes' : 'http://zthes.z3950.org/xml/zthes-05.dtd',
-  'zeerex' : 'http://explain.z3950.org/dtd/2.0/',
-  'rec' : 'info:srw/schema/2/rec-1.0',
-  'xpath' : 'info:srw/schema/1/xpath-1.0'
+  'dc': 'info:srw/schema/1/dc-v1.1',
+  'diag': 'info:srw/schema/1/diagnostic-v1.1',
+  'mods': 'info:srw/schema/1/mods-v3.0',
+  'onix': 'info:srw/schema/1/onix-v2.0',
+  'marcxml': 'info:srw/schema/1/marcxml-v1.1',
+  'ead': 'info:srw/schema/1/ead-2002',
+  'ccg': 'http://srw.o-r-g.org/schemas/ccg/1.0/',
+  'marcsgml': 'http://srw.o-r-g.org/schemas/marcsgml/12.0/',
+  'metar': 'http://srw.o-r-g.org/schemas/metar/1.0/',
+  'unesco': 'http://srw.o-r-g.org/schemas/unesco/1.0/',
+  'zthes': 'http://zthes.z3950.org/xml/zthes-05.dtd',
+  'zeerex': 'http://explain.z3950.org/dtd/2.0/',
+  'rec': 'info:srw/schema/2/rec-1.0',
+  'xpath': 'info:srw/schema/1/xpath-1.0'
 }
 
 contextSetNamespaces = {
-  'cql' : 'info:srw/cql-context-set/1/cql-v1.2',
-  'srw' : 'info:srw/cql-context-set/1/cql-v1.1',
-  'dc' : 'info:srw/cql-context-set/1/dc-v1.1',
-  'bath' : 'http://www.loc.gov/zing/cql/context-sets/bath/v1.1/',
-  'zthes' : 'http://zthes.z3950.org/cql/1.0/',
-  'ccg' : 'http://srw.cheshire3.org/contextSets/ccg/1.1/',
-  'ccg_l5r' : 'http://srw.cheshire3.org/contextSets/ccg/l5r/1.0/',
-  'rec' : 'info:srw/cql-context-set/2/rec-1.0',
-  'net' : 'info:srw/cql-context-set/2/net-1.0'
+  'cql': 'info:srw/cql-context-set/1/cql-v1.2',
+  'srw': 'info:srw/cql-context-set/1/cql-v1.1',
+  'dc': 'info:srw/cql-context-set/1/dc-v1.1',
+  'bath': 'http://www.loc.gov/zing/cql/context-sets/bath/v1.1/',
+  'zthes': 'http://zthes.z3950.org/cql/1.0/',
+  'ccg': 'http://srw.cheshire3.org/contextSets/ccg/1.1/',
+  'ccg_l5r': 'http://srw.cheshire3.org/contextSets/ccg/l5r/1.0/',
+  'rec': 'info:srw/cql-context-set/2/rec-1.0',
+  'net': 'info:srw/cql-context-set/2/net-1.0'
 }
 
 profileNamespaces = {
-  'bath' : 'http://zing.z3950.org/srw/bath/2.0/',
-  'zthes' : ' http://zthes.z3950.org/srw/0.5',
-  'ccg' : 'http://srw.cheshire3.org/profiles/ccg/1.0/',
-  'srw' : 'info:srw/profiles/1/base-profile-v1.1'
+  'bath': 'http://zing.z3950.org/srw/bath/2.0/',
+  'zthes': ' http://zthes.z3950.org/srw/0.5',
+  'ccg': 'http://srw.cheshire3.org/profiles/ccg/1.0/',
+  'srw': 'info:srw/profiles/1/base-profile-v1.1'
 }
 
 extensionNamespaces = {
-  'schemaNegotiation' : 'info:srw/extension/2/schemaNegotiation-1.0',
-  'authenticationToken' : 'info:srw/extension/2/auth-1.0'
+  'schemaNegotiation': 'info:srw/extension/2/schemaNegotiation-1.0',
+  'authenticationToken': 'info:srw/extension/2/auth-1.0'
 }
