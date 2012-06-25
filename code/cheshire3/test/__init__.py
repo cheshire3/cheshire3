377390f50e75d5fa5b12dfc9d1b0a652e548706b
John Harrison <john@bloomonkey.co.uk>
2012-06-25 22:23:31 +0100

    added unittests for QueryFactory
    closes #8

--- code/cheshire3/test/__init__.py
+++ code/cheshire3/test/__init__.py
@@ -1,5 +1,5 @@
 u"""Limited unittests for Cheshire3."""
 
 __all__ = ['testAll', 'testConfigParser', 'testConfigs', 'testDocument',
-           'testParser', 'testPreParser', 'testNormalizer', 'testSession',
-           'testRecord']
+           'testParser', 'testPreParser', 'testQueryFactory', 'testNormalizer',
+           'testSession', 'testRecord']
