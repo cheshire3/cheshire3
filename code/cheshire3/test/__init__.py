800a0bc218d66fbbd6aad39984a3e6b0a7448d92
John Harrison <john.harrison@liv.ac.uk>
2012-06-29 16:27:35 +0100

    added unittest for cheshire.baseStore
    closes #43

--- code/cheshire3/test/__init__.py
+++ code/cheshire3/test/__init__.py
@@ -1,5 +1,5 @@
 u"""Limited unittests for Cheshire3."""
 
-__all__ = ['testAll', 'testConfigParser', 'testConfigs', 'testDocument',
-           'testParser', 'testPreParser', 'testQueryFactory', 'testNormalizer',
-           'testSession', 'testRecord']
+__all__ = ['testAll', 'testBaseStore', 'testConfigParser', 'testConfigs',
+           'testDocument', 'testParser', 'testPreParser', 'testQueryFactory',
+           'testNormalizer', 'testSession', 'testRecord']
