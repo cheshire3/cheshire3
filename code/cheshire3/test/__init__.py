c3df72b2ede2cb2fbdcef9355bafc9e0cc3543fa
John Harrison <john.harrison@liv.ac.uk>
2012-07-02 16:39:39 +0100

    added unittests for BerkeleyDB base DocumentStores

--- code/cheshire3/test/__init__.py
+++ code/cheshire3/test/__init__.py
@@ -1,5 +1,5 @@
 u"""Limited unittests for Cheshire3."""
 
 __all__ = ['testAll', 'testBaseStore', 'testConfigParser', 'testConfigs',
-           'testDocument', 'testParser', 'testPreParser', 'testQueryFactory',
-           'testNormalizer', 'testSession', 'testRecord']
+           'testDocument', 'testDocumentStore', 'testParser', 'testPreParser',
+           'testQueryFactory', 'testNormalizer', 'testSession', 'testRecord']
