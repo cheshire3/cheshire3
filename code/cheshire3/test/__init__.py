ac216d97ad3f1e1bea28170f5518b8f8d5617b9c
John Harrison <john.harrison@liv.ac.uk>
2012-07-03 10:08:33 +0100

    added RecordStores unittests to __all__

--- code/cheshire3/test/__init__.py
+++ code/cheshire3/test/__init__.py
@@ -2,4 +2,5 @@ u"""Limited unittests for Cheshire3."""
 
 __all__ = ['testAll', 'testBaseStore', 'testConfigParser', 'testConfigs',
            'testDocument', 'testDocumentStore', 'testParser', 'testPreParser',
-           'testQueryFactory', 'testNormalizer', 'testSession', 'testRecord']
+           'testQueryFactory', 'testNormalizer', 'testSession', 'testRecord',
+           'testRecordStore']
