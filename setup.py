1c1af1b22f85f8f7c5a1d40d43b8d547848324ae
Cheshire3 <info@cheshire3.org>
2012-07-18 10:37:13 +0100

    incremented version number

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuppath = inspect.getfile(inspect.currentframe())
 setupdir = os.path.dirname(setuppath)
 
 _name = u'cheshire3'
-_version = '1.0.0b42'
+_version = '1.0.0b43'
 
 # Read any necessary bits from README.mdown
 try:
