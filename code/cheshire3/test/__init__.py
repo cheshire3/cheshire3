c8f6dc0d7277e002bd45c3174fdd604934f8b42a
John Harrison <john.harrison@liv.ac.uk>
2012-07-24 12:24:35 +0100

    added unittests for cheshire3.dynamic
    closes #10

--- code/cheshire3/test/__init__.py
+++ code/cheshire3/test/__init__.py
@@ -1,7 +1,19 @@
 u"""Limited unittests for Cheshire3."""
 
-__all__ = ['testAll', 'testBaseStore', 'testConfigParser', 'testConfigs',
-           'testDocument', 'testDocumentStore', 'testNormalizer',
-           'testParser', 'testPreParser', 'testQueryFactory', 'testQueryStore', 
-           'testRecord', 'testRecordStore', 'testSession', 'testTokenizer',
+__all__ = ['testAll',
+           'testBaseStore',
+           'testConfigParser',
+           'testConfigs',
+           'testDocument',
+           'testDocumentStore',
+           'testDynamic',
+           'testNormalizer',
+           'testParser',
+           'testPreParser',
+           'testQueryFactory',
+           'testQueryStore',
+           'testRecord',
+           'testRecordStore',
+           'testSession',
+           'testTokenizer',
            'testTokenMerger']
