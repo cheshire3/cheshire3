666aa9e740d36a66e7060f2d0bb705d775e5c040
John Harrison <john.harrison@liv.ac.uk>
2012-07-24 16:36:23 +0100

    added unittests for SimpleDocumentFactory
    closes #16

--- code/cheshire3/test/testDocumentFactory.py
+++ code/cheshire3/test/testDocumentFactory.py
@@ -11,13 +11,17 @@ try:
 except ImportError:
     import unittest
 
+import os
 import re    
 import string
 
+from tempfile import mktemp
 from lxml import etree
+from collections import Iterable
 
-from cheshire3.documentFactory import SimpleDocumentFactory, \
-ComponentDocumentFactory
+from cheshire3.baseObjects import Document
+from cheshire3.documentFactory import SimpleDocumentFactory,\
+                                      ComponentDocumentFactory
 from cheshire3.record import LxmlRecord
 from cheshire3.test.testConfigParser import Cheshire3ObjectTestCase
 
@@ -27,11 +31,33 @@ class DocumentFactoryTestCase(Cheshire3ObjectTestCase):
     def _get_testData(self):
         return []
 
-    def test_load(self):
-        # Test that load method of an instance returns the instance
-        for args in self._get_testData():
+    def test_loadInstance(self):
+        "Check that load method of an instance returns the instance."
+        tests = self._get_testData()
+        if not list(tests):
+            self.skipTest("No test data defined")
+        for args in tests:
             thing = self.testObj.load(self.session, *args)
             self.assertIsInstance(thing, self._get_class())
+
+    def test_loadIterable(self):
+        "Check that load method of an instance returns an iterable."
+        tests = self._get_testData()
+        if not list(tests):
+            self.skipTest("No test data defined")
+        for args in tests:
+            thing = self.testObj.load(self.session, *args)
+            self.assertIsInstance(thing, Iterable)
+
+    def test_loadedInstance(self):
+        "Check loaded docs are Document instances."
+        tests = self._get_testData()
+        if not list(tests):
+            self.skipTest("No test data defined")
+        for args in tests:
+            self.testObj.load(self.session, *args)
+            for doc in self.testObj:
+                self.assertIsInstance(doc, Document)
 
 
 class SimpleDocumentFactoryTestCase(DocumentFactoryTestCase):
@@ -46,7 +72,75 @@ class SimpleDocumentFactoryTestCase(DocumentFactoryTestCase):
 <subConfig type="documentFactory" id="baseDocumentFactory">
     <objectType>cheshire3.documentFactory.{0}</objectType>
 </subConfig>'''.format(self._get_class().__name__))
-        
+
+    def _init_testData(self):
+        self.testData = td = []
+        # Create  some tempfiles with loadable document content
+        tfn = mktemp('.xml', 'testDocumentFactory')
+        with open(tfn, 'w') as tfh:
+            tfh.write('<meal><spam/><egg/></meal>')
+        td.append((tfn, 0, 'xml'))
+        tfn = mktemp('.xml', 'testDocumentFactory')
+        with open(tfn, 'w') as tfh:
+            tfh.write('<menu><meal><spam/><egg/></meal>'
+                      '<meal><spam/><egg/><sausage/></meal></menu>')
+        td.append((tfn, 1, 'xml'))
+        tfn = mktemp('.xml', 'testDocumentFactory')
+        with open(tfn, 'w') as tfh:
+            tfh.write('<meal><spam/><egg/></meal>')
+        td.append((tfn, 2, 'xml'))
+
+    def _get_testData(self):
+        return self.testData
+
+    def setUp(self):
+        DocumentFactoryTestCase.setUp(self)
+        self._init_testData()
+
+    def tearDown(self):
+        for args in self.testData:
+            try:
+                os.remove(args[0])
+            except OSError:
+                # This load argument not a filename
+                pass
+        DocumentFactoryTestCase.tearDown(self)
+
+    def test_caching(self):
+        "Check that load method with caching works as expected."
+        tests = self._get_testData()
+        if not list(tests):
+            self.skipTest("No test data defined")
+        for args in tests:
+            # At cache level 0 document and locations should both be empty
+            self.testObj.load(self.session, args[0], cache=0)
+            self.assertFalse(self.testObj.docStream.locations)
+            self.assertFalse(self.testObj.docStream.documents)
+            # At cache level 1, documents should be empty
+            # locations should contain items
+            self.testObj.load(self.session, args[0], cache=1)
+            self.assertTrue(self.testObj.docStream.locations)
+            self.assertFalse(self.testObj.docStream.documents)
+            # At cache level 2, locations should be empty
+            # documents should contain items
+            self.testObj.load(self.session, args[0], cache=2)
+            self.assertFalse(self.testObj.docStream.locations)
+            self.assertTrue(self.testObj.docStream.documents)
+
+    def test_tagName(self):
+        "Check that load method with tagName works as expected."
+        tests = self._get_testData()
+        if not list(tests):
+            self.skipTest("No test data defined")
+        for args in tests:
+            if args[2] == 'xml':
+                self.testObj.load(self.session, args[0], tagName="meal")
+                for doc in self.testObj:
+                    self.assertTrue(doc)
+                    docStr = doc.get_raw(self.session)
+                    self.assertTrue(docStr)
+                    self.assertTrue(docStr.startswith('<meal>'))
+
 
 class ComponentDocumentFactoryTestCase(DocumentFactoryTestCase):
     """ComponentDocumentFactory Test Case with simple XPath selectors."""
@@ -105,6 +199,7 @@ class ComponentDocumentFactoryTestCase(DocumentFactoryTestCase):
                    )
 
     def test_componentExtraction(self):
+        "Check for correct component extraction."
         i = 0
         for rec, expectedDocs in self._get_testDataAndExpected():
             rec.id = format(i, "d>0")
@@ -484,7 +579,8 @@ class NSSpanComponentDocumentFactoryTestCase(
 def load_tests(loader, tests, pattern):
     # Alias loader.loadTestsFromTestCase for sake of line lengths
     ltc = loader.loadTestsFromTestCase
-    suite = ltc(ComponentDocumentFactoryTestCase)
+    suite = ltc(SimpleDocumentFactoryTestCase)
+    suite.addTests(ltc(ComponentDocumentFactoryTestCase))
     suite.addTests(ltc(SpanComponentDocumentFactoryTestCase))
     suite.addTests(ltc(KSSpanComponentDocumentFactoryTestCase))
     suite.addTests(ltc(KESpanComponentDocumentFactoryTestCase))
