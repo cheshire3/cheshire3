6b39dcc8c66c2354c1f0a25593603c143bcd417a
John Harrison <john.harrison@liv.ac.uk>
2012-08-01 12:08:06 +0100

    made setup.py PEP8 compliant

--- setup.py
+++ setup.py
@@ -15,14 +15,18 @@ from setuptools import setup, find_packages
 py_version = getattr(sys, 'version_info', (0, 0, 0))
 
 if py_version < (2, 6):
-    warn("Cheshire3 requires Python 2.6 or later; some code may be incompatible with earlier versions.")
+    warn("Cheshire3 requires Python 2.6 or later; some code may be "
+         "incompatible with earlier versions.")
 
 # Requirements
 _install_requires = ['lxml >= 2.1', 'zopyx.txng3.ext >= 3.3.1']
 # Determine python-dateutil version
-dateutilstr = 'python-dateutil == 1.5' if py_version < (3, 0) else 'python-dateutil >= 2.0'
+if py_version < (3, 0):
+    dateutilstr = 'python-dateutil == 1.5'
+else:
+    'python-dateutil >= 2.0'
 _install_requires.append(dateutilstr)
-if py_version < (2,7):
+if py_version < (2, 7):
     _install_requires.append('argparse')
     _install_requires.append('unittest2')
 
@@ -32,7 +36,10 @@ setupdir = os.path.dirname(setuppath)
 
 _name = u'cheshire3'
 _version = '1.0.0b45'
-
+_description = (u'Cheshire3 Search and Retrieval Engine and Information '
+                'Framework')
+_download_url = ('http://www.cheshire3.org/download/{0}/src/{1}-{2}.tar.gz'
+                 ''.format(_version[:5], _name, _version))
 # Read any necessary bits from README.mdown
 try:
     fh = open(os.path.join(setupdir, 'README.mdown'), 'r')
@@ -48,8 +55,8 @@ Description
     desc_end_str = u'''\
 Authors
 -------'''
-    desc_st = fstr.find(desc_st_str)+len(desc_st_str)+1
-    desc_end = fstr.find(desc_end_str)-1
+    desc_st = fstr.find(desc_st_str) + len(desc_st_str) + 1
+    desc_end = fstr.find(desc_end_str) - 1
     _long_description = fstr[desc_st:desc_end]
     # Process any further sections here
     # Delete file contents from memory
@@ -57,25 +64,25 @@ Authors
 
 
 setup(
-    name = _name,
-    version = _version,
-    packages = find_packages('code'),
-    package_dir = {'': 'code'},
-    include_package_data = True,
-    exclude_package_data = {'': ['README.mdown']},
+    name=_name,
+    version=_version,
+    packages=find_packages('code'),
+    package_dir={'': 'code'},
+    include_package_data=True,
+    exclude_package_data={'': ['README.mdown']},
     requires=['lxml(>=2.1)', 'bsddb', 'dateutil', 'argparse'],
     install_requires=_install_requires,
-    dependency_links = [
-    	"http://labix.org/python-dateutil"
-	],
-    extras_require = {
+    dependency_links=[
+        "http://labix.org/python-dateutil"
+    ],
+    extras_require={
         'graph': ['rdflib'],
         'lucene': ['lucene'],
         'sql': ['PyGreSQL >= 3.8.1'],
         'web': ['PyZ3950 >= 2.04']
     },
-    test_suite = "cheshire3.test.testAll",
-    entry_points = {
+    test_suite="cheshire3.test.testAll",
+    entry_points={
         'console_scripts': [
             'cheshire3 = cheshire3.commands.cheshire3_console:main',
             'cheshire3-init = cheshire3.commands.cheshire3_init:main',
@@ -84,15 +91,15 @@ setup(
             'cheshire3-serve = cheshire3.commands.cheshire3_serve:main'
         ],
     },
-    keywords = u"xml document search information retrieval engine data text",
-    description = u'Cheshire3 Search and Retrieval Engine and Information Framework',
-    long_description = _long_description,
-    author = "Rob Sanderson, et al.",
-    author_email = "azaroth@liv.ac.uk",
-    maintainer = 'John Harrison',
-    maintainer_email = u'john.harrison@liv.ac.uk',
-    license = "BSD",
-    classifiers = [
+    keywords=u"xml document search information retrieval engine data text",
+    description=_description,
+    long_description=_long_description,
+    author="Rob Sanderson, et al.",
+    author_email="azaroth@liv.ac.uk",
+    maintainer='John Harrison',
+    maintainer_email=u'john.harrison@liv.ac.uk',
+    license="BSD",
+    classifiers=[
         "Intended Audience :: Developers",
         "Intended Audience :: Information Technology",
         "License :: OSI Approved :: BSD License",
@@ -105,7 +112,6 @@ setup(
         "Topic :: Text Processing :: Linguistic",
         "Topic :: Text Processing :: Markup"
     ],
-    url = "http://www.cheshire3.org/",
-    download_url = 'http://www.cheshire3.org/download/{0}/src/{1}-{2}.tar.gz'.format(
-    _version[:5], _name, _version)
+    url="http://www.cheshire3.org/",
+    download_url=_download_url
 )
