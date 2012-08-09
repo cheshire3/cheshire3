56f86d0f624b17e5f39f46a78b732325ce7e4345
John Harrison <john.harrison@liv.ac.uk>
2012-08-09 16:46:39 +0100

    moved install requirements out to requirements.txt

--- setup.py
+++ setup.py
@@ -39,9 +39,8 @@ else:
     fh.close()
 
 # Requirements
-_install_requires = ['lxml >= 2.1',
-                     'zopyx.txng3.ext >= 3.3.1',
-                     'nltk >= 2.0.2']
+with open(os.path.join(setupdir, 'requirements.txt'), 'r') as fh:
+    _install_requires = fh.readlines()
 # Determine python-dateutil version
 if py_version < (3, 0):
     dateutilstr = 'python-dateutil == 1.5'
