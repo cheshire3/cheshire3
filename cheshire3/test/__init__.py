36798f4fb11003b2b54e9d31a805e695af420bc4
John Harrison <john.harrison@liv.ac.uk>
2012-10-22 14:58:52 +0100

    moved test sub-package README text to module docstring

--- cheshire3/test/__init__.py
+++ cheshire3/test/__init__.py
@@ -1,4 +1,18 @@
-u"""Limited unittests for Cheshire3."""
+u"""Unittests for Cheshire3.
+
+A system like Cheshire3 presents significant challenges to unit testing. 
+Firstly it is designed to be highly configurable to meet specific user 
+requirements - modifying the configuration for a system object will alter 
+it's behavior, and hence will most likely cause unit tests to fail.
+
+Secondly, Cheshire3 is database oriented (a database being a virtual 
+collection of records), meaning that thorough testing of it's processing 
+objects requires the provision of some data and configurations appropriate to 
+it.We will be providing a unittest database for this purpose.
+
+This sub-package therefore provides limited testing of the Cheshire3 framework 
+architecture, and default server configurations.
+"""
 
 __all__ = ['testAll',
            'testBaseStore',
