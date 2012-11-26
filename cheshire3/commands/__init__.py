eb8ec6e6235e0827e7ea0244ec9ee3721734d25c
John Harrison <john.harrison@liv.ac.uk>
2012-11-26 17:20:29 +0000

    refacor: updated cheshire3.commands.__all__

--- cheshire3/commands/__init__.py
+++ cheshire3/commands/__init__.py
@@ -1,9 +1,9 @@
 """Command-line User Interfaces for Cheshire3."""
 
 __all__ = ['cmd_utils',
-           'cheshire3_console',
-           'cheshire3_init',
-           'cheshire3_load',
-           'cheshire3-register',
-           'cheshire3_search',
-           'cheshire3_serve']
+           'console',
+           'init',
+           'load',
+           'register',
+           'search',
+           'serve']
