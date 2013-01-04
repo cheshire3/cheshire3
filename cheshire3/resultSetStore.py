897b81faa9bd733c067eb940ba49421df7d97af7
John Harrison <john.harrison@liv.ac.uk>
2013-01-04 10:55:19 +0000

    style: made cheshire3.resultSetStore PEP8 compliant

--- cheshire3/resultSetStore.py
+++ cheshire3/resultSetStore.py
@@ -1,5 +1,13 @@
 from __future__ import absolute_import
 
+import os
+import sys
+import commands
+import string
+import time
+import types
+import struct
+
 from cheshire3.configParser import C3Object
 from cheshire3.baseObjects import ResultSetStore
 from cheshire3.baseStore import BdbStore
@@ -7,17 +15,27 @@ from cheshire3.resultSet import SimpleResultSet, SimpleResultSetItem
 from cheshire3.exceptions import ConfigFileException
 from cheshire3 import dynamic
 
-import os, types, struct, sys, commands, string, time
+
 NumTypes = [types.IntType, types.LongType]
 
+
 class SimpleResultSetStore(ResultSetStore):
+
     storeHash = {}
     storeHashReverse = {}
     databaseHash = {}
     databaseHashReverse = {}
 
-    _possiblePaths = {'recordStoreHash' : {'docs' : "List of recordStore identifiers. Maps from position in list to int stored."},
-                      'databaseHash' : {'docs' : "List of database identifiers. Maps from position in list to int stored."}}
+    _possiblePaths = {
+        'recordStoreHash': {
+            'docs': ("List of recordStore identifiers. "
+                     "Maps from position in list to int stored.")
+        },
+        'databaseHash': {
+            'docs': ("List of database identifiers. "
+                     "Maps from position in list to int stored.")
+        }
+    }
 
     def __init__(self, session, config, parent):
         ResultSetStore.__init__(self, session, config, parent)
@@ -40,7 +58,7 @@ class SimpleResultSetStore(ResultSetStore):
             # Create a place holder with no information
             expires = self.generate_expires(session, rset)
             if expires:
-                md = {'expires' : expires}
+                md = {'expires': expires}
             else:
                 md = {}
             self.store_data(session, id, "", md)
@@ -50,27 +68,28 @@ class SimpleResultSetStore(ResultSetStore):
             self.store_resultSet(session, rset)
         return id
 
-
     def fetch_resultSet(self, session, id):
         data = self.fetch_data(session, id)
         if (data):
             (cl, srlz) = data.split('||', 1)
-            rset = dynamic.buildObject(session, cl, [])            
+            rset = dynamic.buildObject(session, cl, [])
             rset.deserialise(session, srlz)
             return rset
         else:
             return SimpleResultSet(session, [], id)
 
-
     def store_resultSet(self, session, rset):
         # Serialise resultSet to data + class
-        srlz = rset.serialise(session, pickleOk=self.get_setting(session, 'proxInfo', 1))
+        srlz = rset.serialise(session,
+                              pickleOk=self.get_setting(session,
+                                                        'proxInfo',
+                                                        1)
+                              )
         cl = str(rset.__class__)
-        data = cl + "||" + srlz        
-
+        data = cl + "||" + srlz
         expires = self.generate_expires(session, rset)
         if expires:
-            md = {'expires' : expires}
+            md = {'expires': expires}
         else:
             md = {}
         self.store_data(session, rset.id, data, md)
@@ -81,15 +100,24 @@ class SimpleResultSetStore(ResultSetStore):
 
 
 class BdbResultSetStore(SimpleResultSetStore, BdbStore):
-    _possibleSettings = {'proxInfo' : {'docs' : "Should the result set store maintain proximity information. Defaults to Yes (1), but if this is not needed, it is a significant increase in speed to turn it off (0)", 'type': int}}
-    
+
+    _possibleSettings = {
+        'proxInfo': {
+            'docs': ("Should the result set store maintain proximity "
+                     "information. Defaults to Yes (1), but if this is not "
+                     "needed, it is a significant increase in speed to turn "
+                     "it off (0)"),
+            'type': int
+        }
+    }
+
     storeHash = {}
     storeHashReverse = {}
     databaseHash = {}
     databaseHashReverse = {}
     cxn = None
     txn = None
-    
+
     def __init__(self, session, config, parent):
         self.databaseTypes = ['database', 'expires']
         SimpleResultSetStore.__init__(self, session, config, parent)
@@ -98,4 +126,3 @@ class BdbResultSetStore(SimpleResultSetStore, BdbStore):
 
 class BdbResultSetStore2(BdbResultSetStore):
     pass
-
