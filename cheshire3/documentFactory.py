cheshire3/cheshire3

commit 22561a03dee1ec61c12ae4d62b054e57505dc000

style: renamed variables to avoid re-assignment of built-ins

cheshire3/documentFactory.py
@@ -47,12 +47,12 @@ class BaseDocumentStream:
     startOffset = 0
     endOffset = -1
 
-    def __init__(self, session, stream, format, 
+    def __init__(self, session, stream, format_, 
                  tagName="", codec=None, factory=None):
         self.startOffset = 0
         self.endOffset = -1
         self.factory = factory
-        self.format = format
+        self.format = format_
         if type(tagName) == unicode:
             self.tagName = tagName.encode('utf-8')
         else:
@@ -165,9 +165,9 @@ class XmlDocumentStream(BaseDocumentStream):
     start = None
     endtag = ""
 
-    def __init__(self, session, stream, format,
+    def __init__(self, session, stream, format_,
                  tagName="", codec="", factory=None):
-        BaseDocumentStream.__init__(self, session, stream, format,
+        BaseDocumentStream.__init__(self, session, stream, format_,
                                     tagName, codec, factory)
         if (not tagName):
             tagregex = "<([-a-zA-Z0-9_.]+:)?([-a-zA-Z0-9_.]+)[\s>]"
@@ -353,9 +353,9 @@ class MarcDocumentStream(BaseDocumentStream):
 
 class MultipleDocumentStream(BaseDocumentStream):
 
-    def __init__(self, session, stream, format, 
+    def __init__(self, session, stream, format_, 
                  tagName=None, codec=None, factory=None):
-        BaseDocumentStream.__init__(self, session, stream, format, 
+        BaseDocumentStream.__init__(self, session, stream, format_, 
                                     tagName, codec, factory)
         filterStr = factory.get_setting(session, 
                                         'filterRegexp', 
@@ -587,14 +587,14 @@ class ClusterDocumentStream(BaseDocumentStream):
         sortx = self.factory.get_path(session, 'sortPath', None)
         if sortx is None:
             sortx = getShellResult('which sort')
-        sorted = data + "_SORT"
-        os.spawnl(os.P_WAIT, sortx, sortx, data, '-o', sorted)
+        sortedFn = data + "_SORT"
+        os.spawnl(os.P_WAIT, sortx, sortx, data, '-o', sortedFn)
 
         # Now construct cluster documents.
         doc = ["<cluster>"]
-        f = file(sorted)
+        f = file(sortedFn)
         l = f.readline()
-        # term docid recstore occs (line, posn)*
+        # Term docid recstore occs (line, posn)*
         currKey = ""
         while(l):
             docdata = {}
@@ -648,9 +648,9 @@ class ComponentDocumentStream(BaseDocumentStream):
 
     sources = []
 
-    def __init__(self, session, stream, format, 
+    def __init__(self, session, stream, format_, 
                  tagName=None, codec=None, factory=None):
-        BaseDocumentStream.__init__(self, session, stream, format, 
+        BaseDocumentStream.__init__(self, session, stream, format_, 
                                     tagName, codec, factory)
         self.sources = factory.sources
 
@@ -748,7 +748,7 @@ class ComponentDocumentStream(BaseDocumentStream):
                                                              1)
                     recording = False
                     doc = []
-                    open = []
+                    opened = []
                     closed = []
                     # Walk events (start element, end element etc.)
                     for evt, el in etree.iterwalk(common_ancestor,
@@ -777,16 +777,16 @@ class ComponentDocumentStream(BaseDocumentStream):
                         if recording and isinstance(el.tag, str):
                             if evt in ['start', 'start-ns']:
                                 doc.append(self._make_startTag(el))
-                                open.append(el)
+                                opened.append(el)
                             else:
                                 doc.append(self._make_endTag(el))
-                                if el in open:
-                                    open.remove(el)
+                                if el in opened:
+                                    opened.remove(el)
                                 else:
                                     closed.append(el)
-                    # Close open things
-                    open.reverse()
-                    for el in open:
+                    # Close opened things
+                    opened.reverse()
+                    for el in opened:
                         doc.append(self._make_endTag(el, addTail=False))
                     # Open closed things
                     for el in closed:
@@ -961,18 +961,17 @@ class SimpleDocumentFactory(DocumentFactory):
         self.previousIdx = -1
 
     @classmethod
-    def register_stream(df, format, cls):
-        #df == SimpleDocumentFactory
-        df.streamHash[format] = cls
+    def register_stream(cls, format_, streamClass):
+        cls.streamHash[format_] = streamClass
 
     def load(self, session, data=None, cache=None, 
-             format=None, tagName=None, codec=None):
+             format_=None, tagName=None, codec=None):
         self.loadSession = session
         if data is None:
             data = self.dataPath
 
-        if format is None:
-            format = self.format
+        if format_ is None:
+            format_ = self.format
         if cache is None:
             cache = self.cache
         if tagName is None:
@@ -981,48 +980,48 @@ class SimpleDocumentFactory(DocumentFactory):
             codec = self.codec
 
         # Some laziness checking
-        if not format:
+        if not format_:
             if os.path.exists(data):
                 if data.endswith('.zip'):
-                    format = 'zip'
+                    format_ = 'zip'
                 elif data.endswith('.tar'):
-                    format = 'tar'
+                    format_ = 'tar'
                 elif data.endswith('.xml'):
-                    format = 'xml'
+                    format_ = 'xml'
                 elif data.endswith('.marc'):
-                    format = 'marc'
+                    format_ = 'marc'
                 elif os.path.isdir(data):
-                    format = 'dir'
+                    format_ = 'dir'
             else:
                 if data.startswith("ftp://"):
-                    format = 'ftp'
+                    format_ = 'ftp'
                 elif data.startswith("srb://"):
-                    format = 'srb'
+                    format_ = 'srb'
                 elif data.startswith(("irods://", "rods://")):
-                    format = 'irods'
+                    format_ = 'irods'
                 elif data.startswith(("http://", "https://")):
                     if hasattr(data, '_formatter_parser'):
                         # RDF URIRef
                         data = str(data)
-                    format = "http"
+                    format_ = "http"
                     if data.find('?') > -1:
                         # Parse url and extract param names
                         bits = urlparse.urlsplit(data)
                         plist = [x.split('=')[0] for x in bits[3].split('&')]
                         if 'verb' in plist and 'metadataPrefix' in plist:
-                            format = 'oai'
+                            format_ = 'oai'
                         elif ('operation' in plist and 
                               'version' in plist and 
                               'query' in plist):
-                            format = 'sru'
+                            format_ = 'sru'
 
         try:
-            cls = self.streamHash[format]
+            cls = self.streamHash[format_]
         except KeyError:
             # Just assume single binary data file path
             cls = self.streamHash['file']
 
-        ds = cls(session, data, format, tagName, codec, self)
+        ds = cls(session, data, format_, tagName, codec, self)
         # Store and call generator on first ping
         self.docStream = ds
         self.generator = ds.find_documents(session, cache=cache)
@@ -1114,16 +1113,16 @@ class ComponentDocumentFactory(SimpleDocumentFactory):
 
 
 class AccumulatingStream(BaseDocumentStream):
-    def __init__(self, session, stream, format, 
+    def __init__(self, session, stream, format_, 
                  tagName=None, codec=None, factory=None):
         self.factory = factory
-        self.format = format
+        self.format = format_
         self.tagName = tagName
         self.codec = codec
         # And call accumulate to record stream
-        self.accumulate(session, stream, format, tagName, codec, factory)
+        self.accumulate(session, stream, format_, tagName, codec, factory)
 
-    def accumulate(self, session, stream, format, 
+    def accumulate(self, session, stream, format_, 
                    tagName=None, codec=None, factory=None):
         raise NotImplementedError
 
@@ -1134,7 +1133,7 @@ class AccTransformerStream(AccumulatingStream):
     Transformer should return a string.
     """
 
-    def __init__(self, session, stream, format, 
+    def __init__(self, session, stream, format_,
                  tagName=None, codec=None, factory=None):
         if not factory:
             msg = """\
@@ -1151,10 +1150,10 @@ for AccTransformerStream"""
         self.data = []
 
         # now init the AccStream after discovering txr
-        AccumulatingStream.__init__(self, session, stream, format, 
+        AccumulatingStream.__init__(self, session, stream, format_, 
                                     tagName, codec, factory)
 
-    def accumulate(self, session, stream, format, 
+    def accumulate(self, session, stream, format_, 
                    tagName=None, codec=None, factory=None):
         # stream should be record instance
         doc = self.transformer.process_record(session, stream)
@@ -1167,7 +1166,7 @@ for AccTransformerStream"""
 class AccVectorTransformerStream(AccumulatingStream):
     """Accumulate data to be fed to DM, via a vector transformer."""
 
-    def __init__(self, session, stream, format, 
+    def __init__(self, session, stream, format_, 
                  tagName=None, codec=None, factory=None):
         if not factory:
             msg = """\
@@ -1185,10 +1184,10 @@ for AccTransformerStream"""
         self.vectors = []
         self.totalAttributes = 0
         # now init the AccStream after discovering txr
-        AccumulatingStream.__init__(self, session, stream, format, 
+        AccumulatingStream.__init__(self, session, stream, format_, 
                                     tagName, codec, factory)
 
-    def accumulate(self, session, stream, format, 
+    def accumulate(self, session, stream, format_, 
                    tagName=None, codec=None, factory=None):
         # session should be record instance
         doc = self.transformer.process_record(session, stream)
@@ -1228,20 +1227,20 @@ class AccumulatingDocumentFactory(SimpleDocumentFactory):
         SimpleDocumentFactory.__init__(self, session, config, parent)
 
     def loadMany(self, session, data=None, cache=None, 
-                 format=None, tagName=None, codec=None):
+                 format_=None, tagName=None, codec=None):
         for item in data:
-            self.load(session, item, cache, format, tagName, codec)
+            self.load(session, item, cache, format_, tagName, codec)
         # Return self for workflows, mostly can ignore
         return self
 
     def load(self, session, data=None, cache=None, 
-             format=None, tagName=None, codec=None):
+             format_=None, tagName=None, codec=None):
 
         self.loadSession = session
         if data is None:
             data = self.dataPath
-        if format is None:
-            format = self.format
+        if format_ is None:
+            format_ = self.format
         if cache is None:
             cache = self.cache
         if tagName is None:
@@ -1249,40 +1248,40 @@ class AccumulatingDocumentFactory(SimpleDocumentFactory):
         if codec is None:
             codec = self.codec
         # Some laziness checking
-        if not format:
+        if not format_:
             if os.path.exists(data):
                 if data[-4:] == '.zip':
-                    format = 'zip'
+                    format_ = 'zip'
                 elif data[-4:] == '.tar':
-                    format = 'tar'
+                    format_ = 'tar'
                 elif data[-4:] == '.xml':
-                    format = 'xml'
+                    format_ = 'xml'
                 elif data[-5:] == '.marc':
-                    format = 'marc'
+                    format_ = 'marc'
                 elif os.path.isdir(data):
-                    format = 'dir'
+                    format_ = 'dir'
             else:
                 if data[:6] == "ftp://":
-                    format = 'ftp'
+                    format_ = 'ftp'
                 elif data[:6] == "srb://":
-                    format = 'srb'
+                    format_ = 'srb'
                 elif data[:7] == "http://" or data[:8] == "https://":
-                    format = "http"
+                    format_ = "http"
                     if data.find('?') > -1:
                         # parse url and extract param names
                         bits = urlparse.urlsplit(data)
                         plist = [x.split('=')[0] for x in bits[3].split('&')]
                         if 'verb' in plist and 'metadataPrefix' in plist:
-                            format = 'oai'
+                            format_ = 'oai'
                         elif ('operation' in plist and 
                               'version' in plist and 
                               'query' in plist):
-                            format = 'sru'
+                            format_ = 'sru'
         if not self.docStream:
-            cls = self.streamHash[format]
-            self.docStream = cls(session, data, format, tagName, codec, self)
+            cls = self.streamHash[format_]
+            self.docStream = cls(session, data, format_, tagName, codec, self)
         else:
-            self.docStream.accumulate(session, data, format, 
+            self.docStream.accumulate(session, data, format_, 
                                       tagName, codec, self)
         self.previousIdx = -1
         self.cache = cache
@@ -1354,11 +1353,11 @@ temporarily during subsequent load() calls."""
                 if (child.nodeType == elementType and 
                     child.localName == "map"):
                     t = child.getAttributeNS(None, 'type')
-                    map = []
+                    map_ = []
                     for xpchild in child.childNodes:
                         if (xpchild.nodeType == elementType and 
                             xpchild.localName == "xpath"):
-                            map.append(flattenTexts(xpchild))
+                            map_.append(flattenTexts(xpchild))
                         elif (xpchild.nodeType == elementType and 
                               xpchild.localName == "process"):
                             # Turn xpath chain to workflow
@@ -1379,17 +1378,17 @@ temporarily during subsequent load() calls."""
                                                           xpchild, 
                                                           self)
                                 process._handleConfigNode(session, xpchild)
-                            map.append(process)
+                            map_.append(process)
                     # XXX FIX ME 
-                    # vxp = verifyXPaths([map[0]])
-                    vxp = [map[0]]
-                    if (len(map) < 3):
+                    # vxp = verifyXPaths([map_[0]])
+                    vxp = [map_[0]]
+                    if (len(map_) < 3):
                         # Default ExactExtractor
-                        map.append([['extractor', 'SimpleExtractor']])
+                        map_.append([['extractor', 'SimpleExtractor']])
                     if (t == u'key'):
-                        self.keyMap = [vxp[0], map[1], map[2]]
+                        self.keyMap = [vxp[0], map_[1], map_[2]]
                     else:
-                        maps.append([vxp[0], map[1], map[2]])
+                        maps.append([vxp[0], map_[1], map_[2]])
             self.maps = maps
 
     def _handleLxmlConfigNode(self, session, node):
@@ -1401,10 +1400,10 @@ temporarily during subsequent load() calls."""
                         'type',
                         child.attrib.get('{%s}type' % CONFIG_NS, '')
                     )  
-                    map = []
+                    map_ = []
                     for xpchild in child.iterchildren(tag=etree.Element):
                         if xpchild.tag in ["xpath", '{%s}xpath' % CONFIG_NS]:
-                            map.append(flattenTexts(xpchild).strip())
+                            map_.append(flattenTexts(xpchild).strip())
                         elif xpchild.tag in ["process",
                                              '{%s}process' % CONFIG_NS]:
                             # turn xpath chain to workflow
@@ -1421,20 +1420,20 @@ temporarily during subsequent load() calls."""
                                                           self)
                                 process._handleLxmlConfigNode(session, 
                                                               xpchild)
-                            map.append(process)
+                            map_.append(process)
 
-                    #vxp = [map[0]]
-                    if (len(map) < 3):
+                    #vxp = [map_[0]]
+                    if (len(map_) < 3):
                         # default ExactExtractor
-                        map.append([['extractor', 'SimpleExtractor']])
+                        map_.append([['extractor', 'SimpleExtractor']])
                     if (t == u'key'):
-                        self.keyMap = [map[0], map[1], map[2]]
+                        self.keyMap = [map_[0], map_[1], map_[2]]
                     else:
-                        maps.append([map[0], map[1], map[2]])
+                        maps.append([map_[0], map_[1], map_[2]])
             self.maps = maps
 
     def load(self, session, data=None, cache=None, 
-             format=None, tagName=None, codec=None):
+             format_=None, tagName=None, codec=None):
         # Extract cluster information, append to temp file
         # data must be a record
         p = self.permissionHandlers.get('info:srw/operation/2/cluster', None)
@@ -1452,11 +1451,11 @@ temporarily during subsequent load() calls."""
         raw = rec.process_xpath(session, self.keyMap[0])
         keyData = self.keyMap[2].process(session, [raw])
         fieldData = []
-        for map in self.maps:
-            raw = rec.process_xpath(session, map[0])
-            fd = map[2].process(session, [raw])
+        for map_ in self.maps:
+            raw = rec.process_xpath(session, map_[0])
+            fd = map_[2].process(session, [raw])
             for f in fd.keys():
-                fieldData.append(u"%s\x00%s\x00" % (map[1], f))
+                fieldData.append(u"%s\x00%s\x00" % (map_[1], f))
         d = u"".join(fieldData)
         for k in keyData.iterkeys():
             try:

commit 898ed7e0e107bc89a26b930330b742b8a860d523

bugfix: fixed keyword argument `format` -> `format_` bug
introduced in 22561a03dee1ec61c12ae4d62b054e57505dc000

cheshire3/documentFactory.py
@@ -409,7 +409,7 @@ You could try using zip.'''
         s = self._fetchStream(item)
         if trip[0] == 'stream':
             cls = trip[1]
-            nstream = cls(session, s, format=trip[2], 
+            nstream = cls(session, s, format_=trip[2], 
                           tagName=self.tagName, 
                           codec=self.codec, 
                           factory=self.factory)