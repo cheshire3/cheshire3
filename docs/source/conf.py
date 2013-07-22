c7104b3cb62badd95275402c5ca1786bfa5f10ee
John Harrison <john.harrison@liv.ac.uk>
2013-07-22 14:59:14 +0100

    docs: updated sphinx docs authors and copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Cheshire3'
-copyright = u'2013, Rob Sanderson, et al.'
+copyright = u'2013, University of Liverpool'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -187,7 +187,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'Cheshire3.tex', u'Cheshire3 Documentation',
-   u'Rob Sanderson, et al.', 'manual'),
+   u'Rob Sanderson, John Harrison, et al.', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -217,7 +217,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'cheshire3', u'Cheshire3 Documentation',
-     [u'Rob Sanderson, et al.'], 1)
+     [u'Rob Sanderson, John Harrison, et al.'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -231,7 +231,9 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'Cheshire3', u'Cheshire3 Documentation',
-   u'Rob Sanderson, et al.', 'Cheshire3', 'One line description of project.',
+   u'Rob Sanderson, John Harrison, et al.',
+   'Cheshire3',
+   'Cheshire3 information retrieval and data analysis system.',
    'Miscellaneous'),
 ]
 
