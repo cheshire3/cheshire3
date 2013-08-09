d5c7fbca36ca60e417cc19ee46842e5a772a04cb
John Harrison <john.harrison@liv.ac.uk>
2013-08-09 12:16:48 +0100

    admin: strip EOF char from VERSION.txt in setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ _description = ('Cheshire3 Search and Retrieval Engine and Information '
                 'Framework')
 # Discover version number from file    
 with open(os.path.join(setupdir, 'VERSION.txt'), 'r') as vfh:
-    _version = vfh.read()
+    _version = vfh.read().strip()
 
 _download_url = ('http://download.cheshire3.org/{0}/src/{1}-{2}.tar.gz'
                  ''.format(_version[:3], _name, _version))
