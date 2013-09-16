64af36d35150ddf499cd622e30310c87ea3a9631
John Harrison <john.harrison@liv.ac.uk>
2013-09-16 16:10:28 +0100

    +func: utilities for parsing/opening iRODS URLs

--- cheshire3/grid/irods_utils.py
+++ cheshire3/grid/irods_utils.py
@@ -1,3 +1,14 @@
+"""iRODS Utilities for Cheshire3."""
+
+from urlparse import urlsplit
+from collections import namedtuple
+
+try:
+    import irods
+except ImportError:
+    irods = None
+
+from cheshire3.exceptions import MissingDependencyException
 
 
 def icatValToPy(val, un):
@@ -21,3 +32,71 @@ def pyValToIcat(val):
         return ('%020f' % val, 'float')
     else:
         return (val, 'str')
+
+
+def parse_irodsUrl(url):
+    """Parse and iRODS URL, return a named tuple.
+
+    Return value will have attributes:
+
+    rodsHost
+        Name of the iRODS host
+
+    rodsPort
+        Number of the port on which iRODS is served
+
+    rodsZone
+        Name of iRODS Zone
+
+    path
+        Absolute path of the file/collection
+
+    rodsUserName
+        iRODS username
+
+    rodsHome
+        iRODS home collection of given rodsUserName
+
+    relpath
+        Path relative to rodsHome
+
+    """
+    if irods is None:
+        raise MissingDependencyException("parse_irodsUrl()", 'irods (PyRods)')
+    IrodsUrl = namedtuple("IrodsUrl",
+                          ["rodsHost",
+                           "rodsPort",
+                           "rodsZone",
+                           "path",
+                           "rodsUserName",
+                           "rodsHome",
+                           "relpath"],
+                          verbose=False
+                          )
+    parsed = urlsplit(url)
+    pathParts = parsed.path.split('/')
+    return IrodsUrl(parsed.hostname,
+                    parsed.port,
+                    pathParts[0],             # Zone
+                    parsed.path,              # Absolute path
+                    parsed.username,
+                    '/'.join(pathParts[:4]),  # Home
+                    '/'.join(pathParts[4:])   # Path relative to home
+                    )
+
+
+def open_irodsUrl(url, mode='r'):
+    """Open and return the file specified by an iRODS URL.
+
+    Returns a file-like object - ``irods.IrodsFile``
+    """
+    if irods is None:
+        raise MissingDependencyException("open_irodsUrl()", 'irods (PyRods)')
+    parsed = parse_irodsUrl(url)
+    conn, errMsg = irods.rcConnect(parsed.rodsHost,
+                                   parsed.rodsPort,
+                                   parsed.rodsUserName,
+                                   parsed.rodsZone
+                                   )
+    status = irods.clientLogin(conn)
+    return irods.irodsOpen(conn, parsed.path, mode)
